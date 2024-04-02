f719405ba506b5739a5f83f690b3fb08913e8a14
guilherme <guirossik10@gmail.com>
2024-04-02 19:44:26 -0300

    forms junto com o js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+document.querySelector("#convidados").addEventListener("change", calcular);
+document.querySelector("#tipo").addEventListener("change", calcular);
+document.querySelector("#prototipo_sim").addEventListener("change", calcular);
+document.querySelector("#prototipo_nao").addEventListener("change", calcular);
+document.querySelector("#js").addEventListener("change", calcular);
+
+function calcular() {
+  //DOM = Document object model
+
+  const convidados = document.querySelector("#convidados").value;
+  const tipo = document.querySelector("#tipo").value;
+  const prototipo_sim = document.querySelector("#prototipo_sim").checked;
+  const jscrianca = document.querySelector("#js").checked;
+
+  let valor = tipo == 2 ? convidados * 1000 + 2000 : convidados * 1000;
+
+  if (prototipo_sim) valor += 500;
+
+  if (jscrianca) valor += 10000;
+
+  document.querySelector("#mensagem-valor").innerText = `R$ ${valor.toFixed(
+    2
+  )}`;
+
+  console.log(convidados);
+}
