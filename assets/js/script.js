191ee3d4162d95743c89346c8aeec9be5e1abdbc
Victório <victoriopereira30@gmail.com>
2024-04-09 22:30:24 -0300

    Formulario e JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,38 @@
-document.querySelector("#convidados").addEventListener("change", calcular);
-document.querySelector("#tipo").addEventListener("change", calcular);
-document.querySelector("#prototipo_sim").addEventListener("change", calcular);
-document.querySelector("#prototipo_nao").addEventListener("change", calcular);
-document.querySelector("#js").addEventListener("change", calcular);
+//Tratamento de Eventos
+document.querySelector("#pessoas").addEventListener("change",calcular)
+document.querySelector("#horas").addEventListener("change",calcular)
+document.querySelector("#trezentos").addEventListener("change",calcular)
+document.querySelector("#quatrocentos").addEventListener("change",calcular)
+document.querySelector("#seicentos").addEventListener("change",calcular)
 
-function calcular() {
-  //DOM = Document object model
+document.querySelector("#tempo").addEventListener("input", function() {
+    const tempo = document.querySelector("#tempo").value
+    document.querySelector("label[for=tempo]").innerText = "Tempo: " + tempo + " horas"
+    calcular()
+})
 
-  const convidados = document.querySelector("#convidados").value;
-  const tipo = document.querySelector("#tipo").value;
-  const prototipo_sim = document.querySelector("#prototipo_sim").checked;
-  const jscrianca = document.querySelector("#js").checked;
 
-  let valor = tipo == 2 ? convidados * 1000 + 2000 : convidados * 1000;
+function calcular(){
 
-  if (prototipo_sim) valor += 500;
+  //DOM - Document Object Model
+  const qtdeDePessoas =  document.querySelector("#pessoas").value
 
-  if (jscrianca) {
-    valor += 10000;
-    let localmensagem = document.getElementById("vsf");
-    let mensagem = document.createElement("div");
-    mensagem.innerHTML = "<p>SE FUDEU</p>";
-    localmensagem.appendChild(mensagem);
-  }
+  let valor = 200 + qtdeDePessoas * 100
 
-  document.querySelector("#mensagem-valor").innerText = `R$ ${valor.toFixed(
+  const horas =  document.querySelector("#horas").value
+  if(horas == 2) valor += 100
+  if(horas == 3) valor += 500
+
+  const g300 =  document.querySelector("#trezentos").checked
+  if (g300) valor *= 1
+
+  const g450 =  document.querySelector("#quatrocentos").checked
+  if (g450) valor *= 1.5
+
+  const g600 =  document.querySelector("#seicentos").checked
+  if (g600) valor *= 2
+
+  document.querySelector("#valor").innerText = `R$ ${valor.toFixed(
     2
   )}`;
 
